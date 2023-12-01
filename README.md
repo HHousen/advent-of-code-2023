67b794b14e5d7b2a194e7920db8478f748c41848
Hayden Housen <hayden@haydenhousen.com>
2023-11-30 21:59:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HHousen Advent Of Code 2023
+
+> HHousen's solutions to the 2023 Advent Of Code puzzles at <https://adventofcode.com/2023>.
---
+++ day_1/day1.py
@@ -0,0 +1,14 @@
+with open("puzzle_input.txt", "r") as puzzle_input:
+    puzzle_input = puzzle_input.read()
+
+something = puzzle_input.split("\n\n")
+
+part1_solution = 
+
+# Part 1 Solution: something
+print(f"Part 1 Solution: {part1_solution}")
+
+part2_solution = 
+
+# Part 2 Solution: something
+print(f"Part 2 Solution: {part2_solution}")
