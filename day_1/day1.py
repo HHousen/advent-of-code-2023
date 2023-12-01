deeccc5276e9cc408956c2738b57ad1eeb9ae0cb
Hayden Housen <hayden@haydenhousen.com>
2023-12-01 00:28:39 -0500

    Complete Day 1

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,14 +1,29 @@
 with open("puzzle_input.txt", "r") as puzzle_input:
-    puzzle_input = puzzle_input.read()
+    puzzle_input = puzzle_input.read().splitlines()
 
-something = puzzle_input.split("\n\n")
+p1_total = 0
+p2_total = 0
+for line in puzzle_input:
+    p1_digits = []
+    p2_digits = []
+    for line_idx, character in enumerate(line):
+        if character.isdigit():
+            p1_digits.append(character)
+            p2_digits.append(character)
+        for num_idx, num_string in enumerate(
+            ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        ):
+            if line[line_idx:].startswith(num_string):
+                p2_digits.append(num_idx + 1)
 
-part1_solution = 
+    p1_total += int(f"{p1_digits[0]}{p1_digits[-1]}")
+    p2_total += int(f"{p2_digits[0]}{p2_digits[-1]}")
 
-# Part 1 Solution: something
+part1_solution = p1_total
+part2_solution = p2_total
+
+# Part 1 Solution: 54634
 print(f"Part 1 Solution: {part1_solution}")
 
-part2_solution = 
-
-# Part 2 Solution: something
+# Part 2 Solution: 53855
 print(f"Part 2 Solution: {part2_solution}")
